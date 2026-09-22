e5718572b9be53fe6eaef065a99d82f404fc1f30
EduardoMusulao <eduardomusulao878@gmail.com>
2026-09-22 10:45:05 -0300

    primeiro commit

---
+++ src/io/github/musulaoeduardo/clientes/Main.java
@@ -0,0 +1,10 @@
+package io.github.musulaoeduardo.clientes;
+
+import io.github.musulaoeduardo.clientes.apresentacao.TelaCadastro;
+
+public class Main {
+    public static void main(String[] args) {
+        TelaCadastro telaCadastro = new TelaCadastro();
+        telaCadastro.setVisible(true);
+    }
+}
